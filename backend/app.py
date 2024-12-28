36b48e3e8a9ba6a262057f8796f70e3bf4596195
AlenaKhanis <alen4k245@gmail.com>
2024-12-28 19:23:15 +0200

    Refactor Flask app structure and update templates for improved routing

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+import sys
 from flask import Flask, render_template
 import webbrowser
 import threading
@@ -5,16 +7,32 @@ from routes.material_routes import material_bp
 from routes.print_routes import print_bp
 from models import db
 
-app = Flask(__name__, static_folder='../static', template_folder='../templates')
 
+if getattr(sys, 'frozen', False):
+    
+    template_folder = os.path.join(sys._MEIPASS, 'templates')
+    static_folder = os.path.join(sys._MEIPASS, 'static')
+else:
+   
+    base_dir = os.path.abspath(os.path.dirname(__file__)) 
+    template_folder = os.path.join(base_dir, '..', 'templates') 
+    static_folder = os.path.join(base_dir, '..', 'static')
+
+print(f"Template Folder: {template_folder}")
+print(f"Static Folder: {static_folder}")
+
+
+app = Flask(__name__, static_folder=static_folder, template_folder=template_folder)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
 db.init_app(app)
 
+
 with app.app_context():
-    db.create_all() 
+    db.create_all()
 
 
 app.register_blueprint(material_bp, url_prefix='/api')
@@ -24,13 +42,12 @@ app.register_blueprint(print_bp, url_prefix='/api')
 def add_material():
     return render_template('add_material.html')
 
-
 @app.route('/sales')
 def sales():
     return render_template('sales.html')
 
 @app.route('/')
-def print():
+def print_view():
     return render_template('print.html')
 
 
@@ -38,6 +55,5 @@ def open_browser():
     webbrowser.open('http://localhost:5000')
 
 if __name__ == '__main__':
-
     threading.Timer(1, open_browser).start()
     app.run(debug=True, use_reloader=False)
